3f0b1eb6faacc47afdc2a6612c77399d29f908c4
hannuee <32358926+hannuee@users.noreply.github.com>
2021-08-11 18:01:50 +0300

    fix: page reload as admin loses unavailable products

--- genericstore/src/App.js
+++ genericstore/src/App.js
@@ -1,9 +1,11 @@
 import React, {useEffect} from 'react'
 import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 //import CategoryList from './components/CategoryList'
 import ResponsiveDrawer from './components/ResponsiveDrawer'
 import { initializeCategories } from './reducers/categoryReducer'
 import { initializeAvailableProducts } from './reducers/productReducer'
+import { initializeAllProducts } from './reducers/productReducer'
 
 //import Container from '@material-ui/core/Container'
 
@@ -13,20 +15,29 @@ import { BrowserRouter as Router } from "react-router-dom"
 const App = () => {  
   const dispatch = useDispatch()
 console.log('PITÄIS ALUSTAAAAAAAAAAAAA')
+  const loggedIn = useSelector(state => state.customers.loggedIn)
   useEffect(() => {console.log('ALUSTUSSSSSSSSSSSSSSSS'); dispatch(initializeCategories())}, [dispatch])
-  useEffect(() => dispatch(initializeAvailableProducts()), [dispatch])
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('genericStoreUser')
+
+    let user
     if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
+      user = JSON.parse(loggedUserJSON)
+
       dispatch({
         type: 'ADD_LOGGED_IN',
         data: user
       })
     }
+
+    if (user != null && user.admin != null) dispatch(initializeAllProducts(user.token))
+    else dispatch(initializeAvailableProducts())
   }, [dispatch])
 
+  //useEffect(() => {
+  //  dispatch(initializeAvailableProducts())
+  //}, [dispatch])
 
 
   return (
